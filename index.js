9cb7a3d517c022f44b81e0126f1f7abe5930ab0c
sowmik-sec <sowmik.sec@gmail.com>
2023-04-19 15:15:55 +0600

    create room

--- index.js
+++ index.js
@@ -8,10 +8,20 @@ const expressServer = http.createServer(app);
 const io = new Server(expressServer);
 
 io.on("connection", (socket) => {
-  socket.on("chat", (msg) => {
-    io.emit("chat_transfer", msg);
-  });
+  socket.join("kitchen-room");
+  io.sockets.in("kitchen-room").emit("cooking", "Fried Rice");
+  io.sockets.in("kitchen-room").emit("boiling", "Boiling water");
+
+  socket.join("bed-room");
+  io.sockets.in("bed-room").emit("sleep", `I'm sleeping`);
+  io.sockets.in("bed-room").emit("rest", `I'm taking rest`);
 });
+
+// io.on("connection", (socket) => {
+//   socket.on("chat", (msg) => {
+//     io.emit("chat_transfer", msg);
+//   });
+// });
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
